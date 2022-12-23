a3312c7e129c7552e462d5d0143538a77e987860
infectionfire <infectionfirework@gmail.com>
2022-12-23 18:25:15 +0300

    init commit

---
+++ src/main/java/com/example/rmqConsumer/RMQConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.rmqConsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RMQConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RMQConsumerApplication.class, args);
+    }
+
+}
